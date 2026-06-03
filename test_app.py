c81f108f452a7ea7680f08b14aaba088e9c90faa
D6CFX <poxiao20070316@outlook.com>
2026-06-03 18:04:32 +0800

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -5,4 +5,5 @@ def test_hello_route():
     client = app.test_client()
     response = client.get('/')
     assert response.status_code == 200
-    assert b"DevOps 实验成功" in response.data
+    # 改用字符串判断，或者把中文改成utf-8解码校验
+    assert "DevOps 实验成功" in response.data.decode("utf-8")
